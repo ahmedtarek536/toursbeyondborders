5b9a4ef3693fe8a82399e8255762772aec8b95f3
Ahmed Tarek <123866562+ahmedtarek536@users.noreply.github.com>
2025-09-06 10:58:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-// index.js
-const dotenv = require("dotenv");
-dotenv.config();
-
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
